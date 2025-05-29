43619f6c77b8e4e43863cb7c2e6b4ecaa29d3a01
nafas <nfis71800@gmail.com>
2025-05-29 14:15:40 +0330

    html added

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,3 +1,7 @@
+import { Badge } from "flowbite-react";
+
+
+
 export default function MainPage() {
     return (
         <div>
@@ -23,9 +27,41 @@ export default function MainPage() {
                 </defs>
             </svg>
             <img src="/Group%203.svg" alt={'img'}/>
-            <section>
-                <p> مشاوره </p>
-            </section>
+            <Badge color="gray" size="sm" className="h-16 w-[50%] my-4 ">
+                <img src="/Group 12.svg" alt="img" className="w-[48px] "/>
+                <p> مشاوره تلفنی </p>
+            </Badge>
+            <div className="flex m-4">
+                <span> دسته بندی </span>
+                <img src="/icon-wrapper.svg" alt="img"/>
+                <p> مشاهده همه </p>
+            </div>
+            <Badge color="gray" size="sm" className="h-[52px] w-[82px] ">
+               
+                <p> خانواده  </p>
+            </Badge>
+             <div className="flex m-4">
+                <span> دسته بندی </span>
+                <img src="/icon-wrapper.svg" alt="img"/>
+                <p> مشاهده همه </p>
+            </div>
+        
+            
+             <div color="gray" size="sm" className="  border">
+                <div className="flex">
+                    <p> نیما سلیمانی </p>
+                    <spann> ۴.۷(۲۳۰) </spann>
+                    <img src="/Vector.svg " alt="img"/>
+                </div>
+                <img src="/_Avatar (1).svg" alt="img" className="w-[48px] "/>
+                <p> وکیل پایه یک دادگستری </p>
+                <div className="">
+                    <span> شروع از ۲۰ دقیه </span>
+                    <span> ۱۰۰,۰۰۰ تومان </span>
+                </div>
+            </div>
+            
+
 
         </div>
     )
