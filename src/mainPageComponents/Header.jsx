7a7de0888910e7fbda95ed87db79e7db7e8adb4c
nafas <nfis71800@gmail.com>
2025-05-30 22:54:57 +0330

    profile added

--- src/mainPageComponents/Header.jsx
+++ src/mainPageComponents/Header.jsx
@@ -6,9 +6,9 @@ import YekVakilSvg from "../assets/svg/YekVakilSvg"
 export default function HeaderForMainPage(){
     return(
         <>
-            <div className=" flex relative w-full max-w-md pl-4 mb-5 mt-2">
+            <div className=" flex relative   pl-4 mb-5 mt-2 justify-between">
                 <YekVakilSvg />
-                <input type="search"  placeholder="جست و جو" className=" pr-9 pl-4 py-2 border border-gray-300 rounded-md flex-grow      " />
+                <input type="search"  placeholder="جست و جو" className=" pr-9  py-2 border border-gray-300 rounded-md flex-grow      " />
                  <MagnifierSvg className="absolute right-3 top-1/2 -translate-y-1/2"/>
                   
             </div> 
--- src/mainPageComponents/Profile.jsx
+++ src/mainPageComponents/Profile.jsx
@@ -3,21 +3,34 @@
 export default function Profile(){
     return(
         <>
-        <div color="gray" className="  border">
-                <div className="flex">
-                    <p> نیما سلیمانی </p>
-                    <span> ۴.۷(۲۳۰) </span>
-                    <img src="/Vector.svg " alt="img"/>
-                </div>
-                <img src="/_Avatar (1).svg" alt="img" className="w-[48px] "/>
-                <p> وکیل پایه یک دادگستری </p>
-                <div className="">
-                    <span> شروع از ۲۰ دقیه </span>
-                    <span> ۱۰۰,۰۰۰ تومان </span>
-                </div>
+
+         <div color="gray" className="border p-2  flex items-center w-[356px] h-[104px]">
+        <img src="/_Avatar (1).svg" alt="avatar" className="w-[80px] h-[80px]" />
+
+        <div className="flex flex-col items-center flex-grow">
+          <section className="flex items-center gap-2">
+            <p>نیما سلیمانی</p>
+            <div className="flex items-center gap-1">
+              <span>۴.۷(۲۳۰)</span>
+              <img src="/Vector.svg" alt="star" />
             </div>
-        
+          </section>
+
+          <p className="mt-2">وکیل پایه یک دادگستری</p>
+
+          <div className="flex gap-2 mt-1">
+            <span>شروع از ۲۰ دقیقه</span>
+            <span>۱۰۰,۰۰۰ تومان</span>
+          </div>
+        </div>
+      </div>
+
+    
+      
+
+
+
         
         </>
-    )
+    )  
 }
