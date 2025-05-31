841a8eb345b2b0866a86d783f4d8a7a41e8ef7a7
nafas <nfis71800@gmail.com>
2025-05-31 18:28:53 +0330

    size changed

--- src/mainPageComponents/Sections.jsx
+++ src/mainPageComponents/Sections.jsx
@@ -4,8 +4,8 @@
 export default function Section({ imageSrc, text }){
     return(
          <div className="bg-gray-50 rounded-lg flex items-center gap-3 p-3 h-16 w-full">
-            <p className="text-sm">{text}</p>
-            <img src={imageSrc} alt="icon" className="w-12" />
+            <p className="text-md pr-2">{text}</p>
+            <img src={imageSrc} alt="icon" className="w-16 " />
         </div>
     )
 }
