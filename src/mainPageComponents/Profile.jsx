6a83bbeb96fc347638fdb5517a3b7a6bbab3ac8a
nafas <nfis71800@gmail.com>
2025-06-02 14:23:24 +0330

    profile color changed

--- src/mainPageComponents/Profile.jsx
+++ src/mainPageComponents/Profile.jsx
@@ -8,16 +8,16 @@ export default function Profile(){
                 <img src="/_Avatar (1).svg" alt="avatar"/>
             </div>
             <div className="grid grid-flow-row mx-8">
-                <div className="grid-flow-col "> نیما سلیمانی  </div>
-                <div className="grid-flow-col"> وکیل پایه یک دادگستری </div>
-                <div className="grid-flow-col"> شروع از ۲۰ دقیقه </div>
+                <div className="grid-flow-col text-lg font-bold"> نیما سلیمانی  </div>
+                <div className="grid-flow-col text-gray-500"> وکیل پایه یک دادگستری </div>
+                <div className="grid-flow-col text-gray-500"> شروع از ۲۰ دقیقه </div>
             </div>
             <div className="flex">
                 <div className="grid grid-flow-row ">
-                <div className="grid-flow-col flex"> <spa> ۴.۷(۲۳۰) </spa> <img src="/Vector.svg" alt="star" className="w-[16px] h-[16px]"/>   </div>
-                <div className="grid-flow-col text-start">  ۱۰۰,۰۰۰ تومان   </div>
+                <div className="grid-flow-col flex "> <spa>  <span className="text-gray-300"> (۲۳۰) </span> ۴.۷</spa> <img src="/Vector.svg" alt="star" className="w-[16px] h-[16px]"/>   </div>
+                <div className="grid-flow-col text-start text-gray-500">  ۱۰۰,۰۰۰ تومان   </div>
             </div>
-            </div>
+            </div> 
         </div>
         </>
            
