68588320a75420c8bd540f82b6b29790d2e37cf2
nafas <nfis71800@gmail.com>
2025-06-02 13:04:50 +0330

    category and vakilsection changed

--- src/mainPageComponents/Category.jsx
+++ src/mainPageComponents/Category.jsx
@@ -3,9 +3,9 @@
 export default function Category (){
     return(
         <>
-         <div className="flex m-4 justify-between">
-                <span> دسته بندی </span>
-                <div className="flex ">
+         <div className="flex m-4 justify-between ">
+                <span className="font-bold text-lg"> دسته بندی </span>
+                <div className="flex w-32">
                      <img src="/icon-wrapper.svg" alt="img"/>
                      <p className="mr-2"> مشاهده همه </p>
                 </div>
