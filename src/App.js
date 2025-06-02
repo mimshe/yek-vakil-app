4306ddb00761e17b7d62ba0428aee9d3a4e3efad
nafas <nfis71800@gmail.com>
2025-06-02 13:55:17 +0330

    profile changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-// import MainPage from "./pages/MainPage";
-import Home from './Home/Header';
+import MainPage from "./pages/MainPage";
+// import Home from './Home/Header';
 
 function App() {
     return (
         <div className="max-w-[390px] mx-auto">
             {/*<LoginPage />*/}
            {/* <VerifyPage/>*/}
-           {/* <MainPage/> */}
-           <Home/>
+           <MainPage/>
+           {/* <Home/> */}
 
 
         </div>
